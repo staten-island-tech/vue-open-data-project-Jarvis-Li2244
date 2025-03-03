1d11e775839223d7d033f5e9d49b90409c4d440a
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-03-03 14:02:36 -0500

    aaaaaaaaaaaaaa

---
+++ vue-opendata/src/components/TestingStuff.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const crime = ref("")
+
+async function crimesNYC () {
+  let result = await fetch("https://data.cityofnewyork.us/resource/bqiq-cu78.json")
+  let data = await result.json()
+  crime.value = data.results;
+  console.log(crime)
+}
+
+onMounted(() => {
+  crimesNYC();
+})
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
